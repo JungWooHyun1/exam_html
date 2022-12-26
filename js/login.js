3e5565ce69fbc6451b143c12ced1ef36e1668fe9
JungWooHyun1 <everdark7@naver.com>
2022-12-26 16:24:41 +0900

    1
    1

--- js/login.js
+++ js/login.js
@@ -1,19 +1,19 @@
         const id = document.getElementById('id');
-            const pw = document.getElementById('pw');        
-            const handleLogin = () => {
-                if(id.value === '') {
-                    alert('아이디를 입력하세요');
-                    id.focus();
-                    return false;
-                }
+        const pw = document.getElementById('pw');        
+        const handleLogin = () => {
+            if(id.value === '') {
+                alert('아이디를 입력하세요');
+                id.focus();
+                return false;
+            }
 
-                if(pw.value === '') {
-                    alert('암호를 입력하세요');
-                    pw.focus();
-                    return false;
-                }
+            if(pw.value === '') {
+                alert('암호를 입력하세요');
+                pw.focus();
+                return false;
+            }
 
-                alert('유효성 통과');
-                location.href="/main.html";
+            alert('유효성 통과');
+            location.href="/main.html";
         
         }
